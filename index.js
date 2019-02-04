a48d7f0881e9eb103045f617843ef62f1a174863
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-02-04 03:47:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,12 @@ function longPlaneteerCalls(words) { var word = false
   }
 }
 
-function findTheCheese (foods) {
+function findTheCheese (foods) { var cheese= ["cheddar", "gouda", "camembert"]
+  for(var i=0; i<foods.length; i++)
+  {if (cheese.includes(food[i]))
+{    
+  return foods[i]
+}
+}
+else return "no cheese"
 }
