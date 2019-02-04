abfce924c32739fcaebe16bc40394ffe314b1b1c
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-02-04 04:09:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 function dwarfRollCall(dwarves) {
+return `1. ${dwarves[0]} 2. ${dwarves[1]} 3. ${dwarves[2]}`
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls){ for (var i=0; i<planeteerCalls.length; i++){
+  planeteerCalls[i]=
+  planeteerCalls[i].toUpperCase() + "!"
+}
+return planeteerCalls
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(words) { var word = false
+  for(var i=0; i<words.length; i++)
+  {if (words.length>4){
+    return word= true
+  }
+    return word
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese (foods) { var cheese= ["cheddar", "gouda", "camembert"]
+  for(var i=0; i<foods.length; i++)
+  {if (cheese.includes(foods[i]))
+{
+  return foods[i]
+}
+}
+return "no cheese!"
 }
