361f2c1101a9282cb7c0bd31de8b0f402291b88b
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-02-04 03:43:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves) { return
+  ("1. + ${dwarves[0]} 2+ ${dwarves[1]} 3. + ${dwarves[2]} 4.+ ${dwarves[3]}")
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls){ for (var i=0; i<planeteerCalls.length; i++){
+  planeteerCalls[i]=
+  planeteerCalls[i].toUpperCase() + "!"
+}
+return planeteerCalls
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(words) { var word = false
+  for(var i=0; i<words.length; i++)
+  {if (words.length>4){
+    return word= true
+  }
+    return word
+  }
 }
 
 function findTheCheese (foods) {
